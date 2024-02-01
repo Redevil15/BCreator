ab7b703b34ca6245cc2ca18b07b5ad0a3eada460
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-31 21:14:05 -0600

    Feat: create queries for server actions

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,4 +1,23 @@
-const AgencyPage = () => {
+import { getAuthUserDetails, verifyAndAcceptsInvitation } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs";
+import { getAuth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const AgencyPage = async () => {
+  /* const authUser = await currentUser();
+
+  if (!authUser) {
+    return redirect("/sign-in");
+  } */
+
+  const agencyId = await verifyAndAcceptsInvitation()
+  console.log(agencyId)
+
+  // get users details 
+  const user = await getAuthUserDetails();
+
+
+
   return (
     <div>
       <h1>Agency Page</h1>
